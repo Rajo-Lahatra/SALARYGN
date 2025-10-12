f1bed3aef3e6da68b5e387ca3fc7809c84bd850f
Rajo-Lahatra <lahatrajo@gmail.com>
2025-10-12 20:24:11 +0000

    correction erreur deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Ajouter ici les modules que vous voulez externaliser explicitement
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      // Assurer que les imports sont résolus correctement
+    },
+  },
 })
